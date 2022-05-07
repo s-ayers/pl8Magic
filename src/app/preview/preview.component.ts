4f2513e7869b162146c26b773e39e2930a1ab55d
Steve Ayers <steveneayers@gmail.com>
2022-05-07 10:50:53 -0500

    Uses GraphicFactory from pl8image

--- src/app/preview/preview.component.ts
+++ src/app/preview/preview.component.ts
@@ -1,48 +1,62 @@
 import { Component, OnInit, Input } from '@angular/core';
-// import { Image } from '@s-ayers/pl8image"';
+// import { Image } from '@s-ayers/pl8image'';
 import { DomSanitizer } from '@angular/platform-browser';
 import { ImageService } from '../services/image.service';
 import { Image } from '@s-ayers/pl8image';
+import { GraphicFactory } from '@s-ayers/pl8image';
+import { toBase64String } from '@angular/compiler/src/output/source_map';
 
 @Component({
   selector: 'app-preview',
   templateUrl: './preview.component.html',
-  styleUrls: ['./preview.component.css']
+  styleUrls: ['./preview.component.css'],
 })
 export class PreviewComponent implements OnInit {
   data: any;
   @Input() palettes: object = {};
-  processing: boolean = false;
+  processing = false;
 
   imageData = null;
-  constructor(private sanitizer: DomSanitizer, private imageService: ImageService) { }
+  constructor(
+    private sanitizer: DomSanitizer,
+    private imageService: ImageService
+  ) {}
 
   ngOnInit() {
     console.log(this.data);
-    this.imageService.active.subscribe(image => {
+    this.imageService.active.subscribe((image) => {
       console.log('preview component');
       if (image !== null) {
-
         this.data = image;
         this.processing = true;
-        setTimeout( () => {
+        setTimeout(() => {
           this.preview();
         }, 100);
         // this.preview();
       }
-
     });
   }
 
   async preview() {
-    if (this.data !== null && this.data.palette !== null && this.palettes.hasOwnProperty(this.data.palette)) {
+    if (
+      this.data !== null &&
+      // this.data !== undefined &&
+      this.data.palette !== null &&
+      this.palettes.hasOwnProperty(this.data.palette)
+    ) {
       const palette = this.palettes[this.data.palette].palette;
-      const binImage = await this.data.Orthogonal(palette).toPNG();
+      // const binImage = await this.data.Orthogonal(palette).toPNG();
+      console.log(this.data);
+      const binImage = GraphicFactory.tiles(this.data.tiles, palette, null, 640, 480);
+      const strImage = await binImage.toPNG();
 
-      const sanData = this.sanitizer.bypassSecurityTrustStyle(binImage.toString('base64'));
+      const sanData = this.sanitizer.bypassSecurityTrustStyle(
+        strImage.toString('base64')
+      );
 
-      this.imageData = 'data:image/png;base64,' + sanData['changingThisBreaksApplicationSecurity'];
-
+      this.imageData =
+        'data:image/png;base64,' +
+        sanData['changingThisBreaksApplicationSecurity'];
     }
 
     this.processing = false;
